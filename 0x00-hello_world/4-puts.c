22b29aadccd9aad1644e1d1483b94625e752ee95
uchechina <kimberlyosita467@gmail.com>
2022-09-08 12:57:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,10 @@
 /**
  *main - Entry point
  *Description: 'Output the size of dataTypes'
- 
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-			puts("\"Programming is like building a multilingual puzzle");
-
-						return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
