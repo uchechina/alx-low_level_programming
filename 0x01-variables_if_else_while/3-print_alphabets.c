ed4065ab27ea57b4004d5d0a1828a67ae69ede17
uchechina <kimberlyosita467@gmail.com>
2022-09-09 15:18:56 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:'output aplphabet in small letters and capital letters'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+char upper_ch = 'A';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (upper_ch = 'A'; upper_ch <= 'Z'; upper_ch++)
+putchar(upper_ch);
+putchar('\n');
+return (0);
+}
