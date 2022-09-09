a2238eb65fb0e8989ddb0ffef68a4cf4e40b2972
uchechina <kimberlyosita467@gmail.com>
2022-09-09 14:38:39 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:'output aplphabet in small letters'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
