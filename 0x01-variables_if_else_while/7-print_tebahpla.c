1b1b339e9dd1e3bcd1042378f9ae24cc769c77f9
uchechina <kimberlyosita467@gmail.com>
2022-09-09 15:43:05 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:'output lower case letters in rivers using putchar'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch = 'z';
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
