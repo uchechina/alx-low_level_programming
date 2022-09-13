40a3779bea4a22a106183822e1647838173b3cc2
uchechina <kimberlyosita467@gmail.com>
2022-09-13 13:01:40 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integrers and returns the result
+ * @x: number being added
+ * @y: number being added
+ *y
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+int sum = 0;
+sum = x + y;
+return (sum);
+}
