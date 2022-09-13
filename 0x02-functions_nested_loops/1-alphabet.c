352e408c7be9b047ab4ef11fcebfb76c8b565666
uchechina <kimberlyosita467@gmail.com>
2022-09-13 11:52:47 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 *main- Entry point
 *print_alphabet - print alphabet in lowercase, followed by a new line.
-*Description: print_alphabet in lowercase
+*Description: print alphabet in lowercase
 *Return: Always 0 (Success)
 */
 void print_alphabet(void)
