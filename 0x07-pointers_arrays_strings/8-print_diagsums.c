12a74dd601b3764cdf0dec5d8f4dac04d518b90b
Udeogu-Osita Uchechi Sylvia <106843060+uchechina@users.noreply.github.com>
2022-09-26 22:51:05 +0100

    7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,22 +6,27 @@
  * @a: 2d array of chars
  *@size: number matrix
  *Return: void
-*/
+ */
+
 void print_diagsums(int *a, int size)
 {
-/*Declaring variables*/
-int i;
-int sum1, sum2;
-sum1 = 0;
-sum2 = 0;
+  /*Declaring variables*/
+  int i;
+  int sum1, sum2;
 
-i = 0;
-while (i < size) /*number repetitions*/
-{
-/*sums for diagsums*/
+  sum1 = 0;
+  sum2 = 0;
+
+  i = 0;
+  while (i < size) /*number repetitions*/
+    {
+      /*sums for diagsums*/
 sum1 = sum1 + *(a + i * size + i);
 sum2 = sum2 + *(a + i * size + size - i - 1);
-i++; 
+
+i++; /*add +1*/
 }
+
 printf("%i, %i\n", sum1, sum2);
+
 }
